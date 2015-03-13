bffd89b6f0f7ea011b371dc0940cc4845d56d3b6
Shamray Alexander <im.samuray@gmail.com>
2015-03-13 18:33:49 +0200

    Update index.php

--- incoming/index.php
+++ incoming/index.php
@@ -1,21 +1,29 @@
 <?php
 file_put_contents('data.json', json_encode($_GET+array('timestamp' => time())));
+$connection = (!empty($_GET['connection']) && $_GET['connection'] <= 1 && $_GET['connection'] >= 0) ? $_GET['connection'] : 0;
 
-
-?>settings = {
-  wifi =  {
-    ssid = "ioix-II",
-    pass = "3sL7UGpX"
-  },
-  send = {
-    host = "wc.office.imsamurai.me",
-    ip = "144.76.74.116",
-    path = "/incoming/",
-    template = 'return "&voltage="..voltage.."&ip="..ip'     
-  },
-  sleep = {
-    timeout = 3,
-    wake = 10,
-    enable = false
-  }
+?>settings = { 
+  wifi = {
+  retries = 5,
+  default = <?= (int)$connection; ?>,
+  connections = {{ 
+         ssid = "ioix-II", 
+         pass = "3sL7UGpX" 
+       },
+       { 
+         ssid = "ioix", 
+         pass = "12345qwert" 
+       }}
+  }, 
+  send = { 
+    host = "wc.office.imsamurai.me", 
+    ip = "144.76.74.116", 
+    path = "/incoming/", 
+    template = 'return "&voltage="..voltage.."&ip="..ip.."&connection="..settings.wifi.default' 
+  }, 
+  sleep = { 
+    timeout = 5, 
+    wake = 10, 
+    enable = false 
+  } 
 }
