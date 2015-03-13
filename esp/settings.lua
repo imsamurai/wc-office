03861c314247fab7190205875d0db73bc90807cc
Shamray Alexander <im.samuray@gmail.com>
2015-03-13 16:11:03 +0200

    Update settings.lua

---
+++ esp/settings.lua
@@ -0,0 +1,17 @@
+settings = { 
+  wifi = { 
+    ssid = "ioix-II", 
+    pass = "3sL7UGpX" 
+  }, 
+  send = { 
+    host = "wc.office.imsamurai.me", 
+    ip = "144.76.74.116", 
+    path = "/incoming/", 
+    template = 'return "&voltage="..voltage.."&ip="..ip' 
+  }, 
+  sleep = { 
+    timeout = 5, 
+    wake = 10, 
+    enable = true 
+  } 
+}
